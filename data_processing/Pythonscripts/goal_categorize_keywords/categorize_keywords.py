182bbc46a3bdeebfc04d362b32c8092170539890
Sophie <sophia.alexanian@gmail.com>
2025-01-03 00:37:36 -0500

    fourth try - more accurate

--- data_processing/Pythonscripts/goal_categorize_keywords/categorize_keywords.py
+++ data_processing/Pythonscripts/goal_categorize_keywords/categorize_keywords.py
@@ -9,24 +9,24 @@ model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
 # Define categories with seed words
 categories = {
-    "Geographic Regions": ["Mediterranean", "mediterranean", "Europe", "Middle East", "Caucasus", "caucasus", "nagorno-karabakh", "west", "crimea"],
-    "Political Terms": ["democracy", "election", "policy", "government", "vote", "independence", "revolution", "protest"],
+    "Geographic Regions": ["Mediterranean", "sevan", "mediterranean", "Europe", "Middle East", "karabakh", "Caucasus", "caucasus", "nagorno-karabakh", "west", "crimea"],
+    "Political Terms": ["democracy", "election", "policy", "security", "minister", "normalization", "territorial", "ministry", "corruption", "rights", "civic", "state", "government", "vote", "independence", "revolution", "protest", "justice", "marches"],
     "Politicians": ["Donald Trump", "Trump", "trump", "biden", "putin", "Joe Biden", "Biden", "Macron", "Emmanuel Macron", "Nikol Pashinyan", "Pashinyan", "pashinyan", "macron", "Ilham Aliyev", "Putin", "aliyev", "ilham", "Kocharyan", "erdogan"],
     "Alliances": ["EU", "EEU", "CSTO", "NATO"],
-    "Organizations": ["ARS", "AGBU", "ARF", "AYF", "ANCA"],
-    "Food": ["manti", "cucumber", "apricot", "liquor", "vodka", "cognac", "wine", "bread", "cheese", "lettuce", "recipe", "spices", "pilaf", "rice", "sarma", "dolma", "peach", "cook"],
-    "Education": ["school", "learn", "literacy", "education", "writing", "reading", "language", "teacher", "math", "teach", "students", "study", "university"],
+    "Organizations": ["ARS", "AGBU", "ARF", "AYF", "ANCA", "homenetmen", "hamazkayin"],
+    "Food": ["manti", "cucumber", "lavash", "kibbeh", "bakery","apricot", "liquor", "vodka", "cognac", "wine", "bread", "dinner", "lunch", "breakfast", "cheese", "lettuce", "recipe", "spices", "pilaf", "rice", "sarma", "dolma", "peach", "cook"],
+    "Education": ["school", "graduation", "curriculum", "youth", "graduates", "children", "learn", "literacy", "education", "writing", "reading", "language", "teacher", "math", "teach", "students", "study", "university"],
     "Nationalities": ["armenian", "lebanese", "polish", "spanish", "slavic", "ukrainian", "turkish", "turkic", "syrian", "azeri", "azerbaijani", "israeli", "palestinian", "russian", "assyrians", "french", "german", "greek", "european", "american", "iranian", "swedish", "swiss"],
-    "Cities": ["gyumri", "boston", "chicago", "ani", "van", "beirut", "aleppo", "yerevan", "Baku", "paris", "berlin", "moscow", "toronto", "watertown", "tel aviv", "jerusalem", "haifa", "nyc", "los angeles", "glendale"],
-    "Countries": ["armenia", "spain", "poland", "lebanon", "ukraine", "syria", "turkey", "azerbaijan", "canada", "australia", "israel", "palestine", "germany", "france", "russia", "china", "usa", "america", "artsakh", "georgia", "sweden", "greece", "abkhazia", "kosovo"],
-    "Genocide": ["genocide", "1915", "recognition", "Talaat Pasha", "massacres", "martyrs", "holocaust"],
-    "Religion": ["church", "faith", "prophet", "lent", "diocese", "archbishop", "prayer", "Christianity", "Islam", "Christian", "Jew", "ministry", "Catholic", "apostolic", "Jesus", "Christ", "cross", "missionaries", "spiritual", "holy", "buddhist", "biblical", "worship", "gospel", "sermon", "reverend", "friar", "pastor", "evangelical","Christmas", "Easter", "Saint"],
-    "Culture": ["poetry", "art", "culture", "literature", "dance", "music", "song", "book"],
-    "Blockade and Conflict": ["blockade", "corridor", "military", "troops", "soldiers", "weapons", "conflict", "war", "shootings", "wounded", "starvation"],
-    "Social Issues": ["charity", "humanitarian", "aid", "fundraiser"],
-    "Social Services": ["healthcare", "education", "public transit", "welfare", "subsidies", "fares", "transporatation", "roads"],
-    "Economic Terms": ["tax", "economic", "jobs", "spending", "budget", "dollar"],
-    "Women's Issues": ["menstrual", "domestic", "pregnancy", "maternity", "feminine", "woman", "mother", "motherinlaw", "infertility"],
+    "Cities": ["gyumri", "boston", "damascus", "chicago", "ani", "van", "beirut", "aleppo", "yerevan", "Baku", "paris", "berlin", "moscow", "toronto", "watertown", "tel aviv", "jerusalem", "haifa", "nyc", "los angeles", "glendale"],
+    "Countries": ["armenia", "spain", "poland", "lebanon", "ukraine", "syria", "turkey", "azerbaijan", "Azerbaijan", "canada", "australia", "israel", "palestine", "germany", "france", "russia", "china", "usa", "america", "artsakh", "georgia", "sweden", "greece", "abkhazia", "kosovo"],
+    "Genocide": ["genocide", "deportees", "1915", "recognition", "Talaat Pasha", "massacres", "martyrs", "holocaust"],
+    "Religion": ["church", "faith", "catholicos", "holiness", "prophet", "orthodox", "secular", "atheist", "lent", "diocese", "archbishop", "prayer", "Christianity", "Islam", "Christian", "Jew", "ministry", "Catholic", "apostolic", "Jesus", "Christ", "cross", "missionaries", "spiritual", "holy", "buddhist", "biblical", "worship", "gospel", "sermon", "reverend", "friar", "pastor", "evangelical","Christmas", "Easter", "Saint"],
+    "Culture": ["poetry", "art", "exhibits", "heritage", "cultural", "books", "musuem", "painting", "sculpture", "culture", "literature", "dance", "music", "song", "book", "comedy", "performance", "humor"],
+    "Blockade and Conflict": ["blockade", "trauma", "aggression", "ceasefire", "munitions", "bombs", "missile", "corridor", "military", "troops", "soldiers", "weapons", "conflict", "war", "battle", "peace", "shootings", "wounded", "starvation", "refugees"],
+    "Social Issues": ["charity", "humanitarian", "aid", "volunteer", "fundraiser", "organizes", "awareness", "activism", "lobbying", "activists", "organizing", "lobbyist"],
+    "Social Services": ["healthcare", "housing", "education", "public transit", "welfare", "subsidies", "childcare", "fares", "transporatation", "roads"],
+    "Economic Terms": ["tax", "labor", "industry", "tech", "technology", "tourism", "manufacturing", "layoffs", "recession", "cuts", "employees", "unemployment", "unions", "workers", "gdp", "debt", "bank", "dram", "economic", "jobs", "employment", "spending", "budget", "dollar", "revenue", "productivity", "fund", "market", "economy", "economist", "economics", "trade"],
+    "Gender Issues": ["menstrual", "girls", "family", "disability", "adoption", "infants", "babies", "teenagers", "gay", "lgbtq", "homophobia", "queer", "domestic", "pregnancy", "maternity", "feminine", "woman", "mother", "motherinlaw", "infertility"],
 }
 
 # Precompute category embeddings
@@ -36,7 +36,7 @@ category_vectors = {
 }
 
 # Similarity threshold for categorizing a keyword
-SIMILARITY_THRESHOLD = 0.4  
+SIMILARITY_THRESHOLD = 0.34  
 
 def categorize_keywords(keywords, category_vectors, similarity_threshold=SIMILARITY_THRESHOLD):
     """
@@ -76,7 +76,7 @@ keywords = df["Keywords"].dropna().tolist()  # Remove NaN values and convert to 
 categorized_keywords = categorize_keywords(keywords, category_vectors)
 
 # Define output directory and ensure it exists
-output_dir = "./output3/"
+output_dir = "./output4/"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
